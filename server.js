476963121b518355f86fb2635dd46229a7daff69
Lauren Simineau <lsimine1@charlotte.edu>
2025-02-08 19:31:39 -0500

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 // Budget API
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use('/', express.static('public'));
 
 const budget = {
     myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+    {
+        title: 'Eat Out',
+        budget: 25
+    },
+    {
+        title: 'Rent',
+        budget: 375
+    },
+    {
+        title: 'Grocery',
+        budget: 110
+    }
+]};
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!')
+});
 
 
 app.get('/budget', (req, res) => {
@@ -30,5 +32,5 @@ app.get('/budget', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+    console.log('Example app listening at http://localhost:3000')
 });
