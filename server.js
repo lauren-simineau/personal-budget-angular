d49ac43bdd1593864f6e9c4f528f9f9bf4df175b
Lauren Simineau <lsimine1@charlotte.edu>
2025-02-09 14:32:41 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,34 +1,28 @@
 // Budget API
-
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat Out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    }
-]};
+app.use('/', express.static('public'));
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!')
 });
 
-
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    const filePath = path.join(__dirname, 'budget-data.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.log("Error reading budget-data.json");
+        }
+        const myBudget = JSON.parse(data);
+        res.json(myBudget);
+});
 });
 
 app.listen(port, () => {
